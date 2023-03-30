98a1471dcd8ec4f8b1b46096707670e9f2f7e010
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-03-30 10:54:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let textinput = document.querySelector("#evaluatedText");
 let countdisplay = document.querySelector("#wordCount");
 textinput.addEventListener("input",()=>{
   let str = textinput.value.trim();
+  if(str==""){
+    countdisplay.innerText = 0;
+    return;
+  }
   let count = str.split(" ").length;
   console.log(str.split(" "));
   countdisplay.innerText = count;

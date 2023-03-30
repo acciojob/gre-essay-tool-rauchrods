cc7ce1230b23584caa6a42974521854ea7c73783
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-03-30 10:50:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 let textinput = document.querySelector("#evaluatedText");
 let countdisplay = document.querySelector("#wordCount");
 textinput.addEventListener("input",()=>{
-  let str = textinput.value;
+  let str = textinput.value.trim();
   let count = str.split(" ").length;
+  console.log(str.split(" "));
   countdisplay.innerText = count;
 });
